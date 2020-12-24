52f698ec89ffbd1d5cb53b9e6c226f25b92490a1
Alexandr <31684774+aparnev@users.noreply.github.com>
2020-12-24 16:47:40 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import spiral from './123.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={spiral} className="App-logo" alt="-" />
+        <p>
+        Hello World!
+        </p>         
+      </header>
+    </div>
+  );
+}
+
+export default App;
